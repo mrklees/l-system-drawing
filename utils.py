4c71329fcd0c006686ab8465eb0517885d082376
mrklees <perusse.a@gmail.com>
2021-04-30 16:28:42 -0700

    improved colors

--- utils.py
+++ utils.py
@@ -28,13 +28,21 @@ def rule(sequence, system_rules):
         return system_rules[sequence]
     return sequence
 
-def draw_l_system(turtle, SYSTEM_RULES, seg_length, angle, palette="blue_pink_grad"):
+def sequenced_gradient(colors, size=60):
+    colors_per_pair = int(size / len(colors)) + 1
+    sequenced_pallete = []
+    for ix in range(1, len(colors)):
+        sequenced_pallete += list(Color(colors[ix - 1]).range_to(Color(colors[ix]), colors_per_pair))
+    return sequenced_pallete
+
+def draw_l_system(turtle, SYSTEM_RULES, seg_length, angle, palette="blue_pink_grad_var"):
     stack = []
     palettes = {
         "lavender_pink_grad": list(Color("#9D6079").range_to(Color("#FA5770"), 60)),
-        "blue_pink_grad": list(Color("#7eb8da").range_to(Color("#ffa5d8"), 60))
+        "blue_pink_grad": list(Color("#7eb8da").range_to(Color("#ffa5d8"), 60)),
+        "blue_pink_grad_var": sequenced_gradient(["#7eb8da", "#92ddea", "#be9ddf", "#ffa5d8"], 60)
     }
-    palette_iter = iter(palettes[palette])
+    palette_iter = palettes[palette]
 
     color_change_ix = len(SYSTEM_RULES) // len(palettes[palette])
 
@@ -42,7 +50,7 @@ def draw_l_system(turtle, SYSTEM_RULES, seg_length, angle, palette="blue_pink_gr
         turtle.pd()
         
         if (ix % color_change_ix == 0) and (any(palette_iter)):
-            next_color = next(palette_iter)
+            next_color = palette_iter.pop()
             turtle.pencolor(f"{next_color}")      
         
         if command in ["F", "G", "R", "L"]:
